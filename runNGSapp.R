ea18af795e712c75dbc5f6101b4bbc1e1d364948
Lennart Kester <lennartkester@pmc-mac0030.fritz.box>
2020-05-12 14:26:52 +0200

    script to launch to NGS app

---
+++ runNGSapp.R
@@ -0,0 +1,7 @@
+## Run PMC NGS tools App ##
+
+## to open the app click Run App button in top right corner of this window ##
+setwd("G:/Diagnostisch Lab/Laboratorium/Moleculair/Patientenuitslagen/NGS_Rtools/")
+library(shiny)
+runApp("NGSapp")
+
