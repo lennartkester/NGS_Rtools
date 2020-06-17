79e59d71f1ead8419e136e846164071190309d49
Lennart Kester <lennartkester@gmail.com>
2020-06-17 20:32:19 +0200

    Added plot expression class function

--- expressionClass.R
+++ expressionClass.R
@@ -16,22 +16,15 @@ generateUmapData <- function(refCohort,nFeatures=5000,nComp=100){
     varGenes <- apply(dataLog,1,var)
     meanGenes <- apply(dataLog,1,mean)
     
-    varFeatures <- names(varGenes)[order(varGenes,decreasing = T)][c(1:5000)]
+    varFeatures <- names(varGenes)[order(varGenes,decreasing = T)][c(1:2000)]
     
     ## scale data (subtract mean and divide by variance) and store scaling factors ##
     dataScale <- apply(dataLog,2,function(x) (x-meanGenes)/varGenes)
     
-    
     ## perform PCA and store scaling factors ##
     pr <- prcomp(t(dataScale[varFeatures,]))
     ## to transform new sample do : ((log(newSample+1)-meanGenes)/varGenes)[varFeatures,] %*% pr$rotation
-    
-    ## check sdev of principal components ##
-    plot(pr$sdev[c(1:100)])
-    ## plot first 2 principal components ##
-    plot(pr$x[,1],pr$x[,2])
-    
-    
+
     dataUmap <- umap(pr$x[,c(1:100)],alpha=1,gamma=1)
     umapData <- cbind(rownames(dataUmap$layout),dataUmap$layout,refCohort$metaData)
     colnames(umapData) <- c("PMABM","Dim1","Dim2","Tumortype","Fusion","HIX")
@@ -48,27 +41,21 @@ newSampleCoordinates <- function(classData,newSample){
   predict(classData$umapFull,t(pr[c(1:100)]))
 }
 
+plotExpressionClass <- function(umapData,classData,tumorTypes,input,scalePoints=1){
+  cols <- rainbow(6)
+  plot(umapData$Dim1,umapData$Dim2,pch=20,xlab="Dim1",ylab="Dim2",cex=2.5*scalePoints,col='lightgrey')
+  points(umapData$Dim1,umapData$Dim2,pch=20,cex=2*scalePoints,col='darkgrey')
+  for ( i in 1:length(tumorTypes)){
+    points(umapData[umapData$Tumortype == tumorTypes[i],"Dim1"],umapData[umapData$Tumortype == tumorTypes[i],"Dim2"],col=cols[i],pch=20,cex=2*scalePoints)
+  }
+  if(input$umapSample != "First select seq run"){
+    newData <- read.csv(paste0(baseDirWTS,input$umapDir,"/expressionData/",input$umapDir,"_counts.csv"),sep="\t")
+    newCoord <- newSampleCoordinates(classData = classData,newSample = newData[,input$umapSample])
+    points(newCoord,pch=20,cex=3*scalePoints,col='black')
+    legend("bottomright",legend=c(input$umapSample,tumorTypes),col=c("black",cols),pch=20,bty='n')
+  }else{
+    legend("bottomright",legend=tumorTypes,col=cols,pch=20,bty='n')
+  }
+}
 
-
-
-
-
-knn(classData$princomp$x[,c(1:100)],pr)
-pr <- apply(newData,2,function(newSample) ((log(((newSample/sum(newSample))*1000000)+1)-classData$meanGenes)/classData$varGenes)[classData$varFeatures] %*% classData$princomp$rotation)
-# 
-# 
-# cols <- rep("black",ncol(dataScale))
-# cols[refCohort$metaData$`Tumor type simple` == "B-ALL"] <- 'red'
-# cols[refCohort$metaData$`Tumor type simple` == "T-ALL"] <- 'blue'
-# cols[refCohort$metaData$`Tumor type simple` == "Neuroblastoma"] <- 'green'
-# cols[refCohort$metaData$`Tumor type simple` == "Burkitt lymphoma"] <- 'purple'
-# cols[refCohort$metaData$`Tumor type simple` == "Pilocytic astrocytoma"] <- 'orange'
-# cols[refCohort$metaData$`Tumor type simple` == "AML"] <- 'lightblue'
-# cols[refCohort$metaData$`Tumor type simple` == "Hodgkin lymphoma"] <- 'pink'
-# 
-# 
-# pdf("20200615_classPlotExample.pdf")
-# plot(dataUmap$layout,col=cols,pch=20)
-# legend("bottomleft",pch=20,bty='n',legend=c("B-ALL","T-ALL","Neuroblastoma","Burkitt lymphoma","Pilocytic astrocytoma","AML","Hodgkin lymphoma"),col=c('red','blue','green','purple','orange','lightblue','pink'))
-# dev.off()
 
