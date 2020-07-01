df28132ec8fe6612947b88ac8b6ab30de8d8df99
Lennart Kester <lennartkester@gmail.com>
2020-07-01 08:12:50 +0200

    Added predictClass functions

--- expressionClass.R
+++ expressionClass.R
@@ -23,12 +23,35 @@ generateUmapData <- function(refCohort,nFeatures=5000,nComp=100){
     
     ## perform PCA and store scaling factors ##
     pr <- prcomp(t(dataScale[varFeatures,]))
+    prData <- pr$x
     ## to transform new sample do : ((log(newSample+1)-meanGenes)/varGenes)[varFeatures,] %*% pr$rotation
 
     dataUmap <- umap(pr$x[,c(1:nComp)],alpha=1,gamma=1)
     umapData <- cbind(rownames(dataUmap$layout),dataUmap$layout,refCohort$metaData)
     colnames(umapData) <- c("PMABM","Dim1","Dim2","Tumortype","Fusion","HIX")
     umapAll <- list("umapData"=umapData,"princomp"=pr,"meanGenes"=meanGenes,"varGenes"=varGenes,"varFeatures"=varFeatures,"umapFull"=dataUmap)
+    
+    umapData$Tumortype <- as.factor(umapData$Tumortype)
+    
+    kMat <- as.data.frame(matrix(ncol=2,nrow=100))
+    samplesTrainAll <- list()
+    for ( j in c(1:100)){
+      set.seed(j)
+      samplesTrain <- unique(sample(rownames(prData),replace = T))
+      samplesTrainAll[[j]] <- samplesTrain
+      prdataTrain <- as.data.frame(prData[samplesTrain,c(1:100)])
+      prdataTrain$class <- umapData[samplesTrain,"Tumortype"]
+      #prdataTest <- as.data.frame(pr$x[samplesTest,c(1:100)])
+      names(prCurSample) <- colnames(prdataTrain)[c(1:100)]
+      #k[[j]] <- kknn(class~., prdata,as.data.frame(prCurSample),distance = 2,kernel = "optimal",scale=F,k=10)
+      kTrain <- train.kknn(class~., prdataTrain,distance = 2,kernel = "optimal",scale=F,kmax = 20)
+      kMat[j,1] <- kTrain$best.parameters$k
+      kMat[j,2] <- kTrain$best.parameters$kernel
+      
+    }
+    trainFreqs <- table(unlist(samplesTrainAll))
+    umapAll$kMat <- kMat
+    umapAll$trainFreqs <- trainFreqs
     
     saveRDS(umapAll,paste0(baseDir,sub(".csv","_umapData.rds",refCohort$version)))
     
@@ -41,21 +64,87 @@ newSampleCoordinates <- function(classData,newSample){
   predict(classData$umapFull,t(pr[c(1:100)]))
 }
 
-plotExpressionClass <- function(umapData,classData,tumorTypes,input,scalePoints=1){
+newSamplePrinComp <- function(classData,newSample){
+  ((log(((newSample/sum(newSample))*1000000)+1)-classData$meanGenes)/classData$varGenes)[classData$varFeatures] %*% classData$princomp$rotation
+}
+
+plotExpressionClass <- function(umapData,classData,tumorTypes,umapDir=NULL,umapSample=NULL,scalePoints=1,neighbours=NULL){
   cols <- rainbow(6)
   plot(umapData$Dim1,umapData$Dim2,pch=20,xlab="Dim1",ylab="Dim2",cex=2.5*scalePoints,col='lightgrey')
   points(umapData$Dim1,umapData$Dim2,pch=20,cex=2*scalePoints,col='darkgrey')
   for ( i in 1:length(tumorTypes)){
     points(umapData[umapData$Tumortype == tumorTypes[i],"Dim1"],umapData[umapData$Tumortype == tumorTypes[i],"Dim2"],col=cols[i],pch=20,cex=2*scalePoints)
   }
-  if(input$umapSample != "First select seq run"){
-    newData <- read.csv(paste0(baseDirWTS,input$umapDir,"/expressionData/",input$umapDir,"_counts.csv"),sep="\t")
-    newCoord <- newSampleCoordinates(classData = classData,newSample = newData[,input$umapSample])
-    points(newCoord,pch=20,cex=3*scalePoints,col='black')
-    legend("bottomright",legend=c(input$umapSample,tumorTypes),col=c("black",cols),pch=20,bty='n')
+  if(!is.null(umapSample)){
+    if(umapSample %in% umapData$PMABM){
+      points(umapData[umapData$PMABM == umapSample,"Dim1"],umapData[umapData$PMABM == umapSample,"Dim2"],pch=20,cex=3*scalePoints,col='black')
+    }else{
+      checkExpressionData(umapDir)
+      newData <- read.csv(paste0(baseDirWTS,umapDir,"/expressionData/",umapDir,"_counts.csv"),sep="\t")
+      newCoord <- newSampleCoordinates(classData = classData,newSample = newData[,umapSample])
+      points(newCoord,pch=20,cex=3*scalePoints,col='black')
+    }
+    legend("bottomright",legend=c(umapSample,tumorTypes),col=c("black",cols),pch=20,bty='n')
   }else{
-    legend("bottomright",legend=tumorTypes,col=cols,pch=20,bty='n')
+    if(!is.null(tumorTypes)){
+      legend("bottomright",legend=tumorTypes,col=cols,pch=20,bty='n')
+    }
+  }
+  if(!is.null(neighbours)){
+    neighbourPoints <- umapData[names(neighbours)[neighbours > 0],]
+    neighbourPoints$neighbour <- neighbours[neighbours > 0]
+    neighbourPoints <- neighbourPoints[order(neighbourPoints$neighbour),]
+    cols <- rev(terrain.colors(101))
+    neighbourPoints$col <- cols[round(neighbourPoints$neighbour*100,0)+1]
+    points(neighbourPoints$Dim1,neighbourPoints$Dim2,col=neighbourPoints$col,pch=20,cex=2)
+    legendRange <- seq((min(umapData$Dim1)+max(umapData$Dim1))-((max(umapData$Dim1)-min(umapData$Dim1))*0.1),(min(umapData$Dim1)+max(umapData$Dim1))+((max(umapData$Dim1)-min(umapData$Dim1))*0.1),length.out = 101)
+    legendy <- min(umapData$Dim2)
+    for(i in 1:100){
+      lines(x = legendRange[c(i,(i+1))],rep(legendy,2),lwd=10,col=rev(terrain.colors(100))[i])
+    }
+    text(x=legendRange[1],y=legendy+((max(umapData$Dim2)-min(umapData$Dim2))*0.025),labels = "0")
+    text(x=legendRange[101],y=legendy+((max(umapData$Dim2)-min(umapData$Dim2))*0.025),labels = "1")
+    text(x=legendRange[50],y=legendy+((max(umapData$Dim2)-min(umapData$Dim2))*0.025),labels = "Similarity")
   }
 }
+
+predictClass <- function(input,classData){
+  checkExpressionData(input$umapDir)
+  newData <- read.csv(paste0(baseDirWTS,input$umapDir,"/expressionData/",input$umapDir,"_counts.csv"),sep="\t")
+  prCurSample <- newSamplePrinComp(classData = classData,newSample = newData[,input$umapSample])
+  prData <- classData$princomp$x
+  if(input$umapSample %in% rownames(prData)){
+    prData <- prData[rownames(prData) != input$umapSample,]
+  }
+  classData$umapData$Tumortype <- as.factor(classData$umapData$Tumortype)
+  k <- list()
+  #samplesTrainAll <- list()
+  neighbourMat <- matrix(ncol=nrow(prData),nrow=100)
+  colnames(neighbourMat) <- rownames(prData)
+  for ( j in c(1:100)){
+    set.seed(j)
+    samplesTrain <- unique(sample(rownames(prData),replace = T))
+    #samplesTrainAll[[j]] <- samplesTrain
+    prdataTrain <- as.data.frame(prData[samplesTrain,c(1:100)])
+    prdataTrain$class <- classData$umapData[samplesTrain,"Tumortype"]
+    #prdataTest <- as.data.frame(pr$x[samplesTest,c(1:100)])
+    names(prCurSample) <- colnames(prdataTrain)[c(1:100)]
+    #k[[j]] <- kknn(class~., prdata,as.data.frame(prCurSample),distance = 2,kernel = "optimal",scale=F,k=10)
+    #kTrain <- train.kknn(class~., prdataTrain,distance = 2,kernel = "optimal",scale=F,kmax = 20)
+    #print(paste(kTrain$best.parameters$k,kTrain$best.parameters$kernel))
+    k[[j]] <- kknn(class~., prdataTrain,as.data.frame(prCurSample),distance = 2,kernel = as.character(classData$kMat$V2[j]),scale=F,k=classData$kMat$V1[j])
+    rownames(k[[j]]$prob) <- input$umapSample
+    nb <- as.vector(k[[j]]$W)
+    names(nb) <- rownames(prdataTrain)[k[[j]]$C]
+    neighbourMat[j,names(nb)] <- nb
+    #neighbours[[j]] <- nb
+  }
+  res <- Reduce('+',lapply(k,function(x) x$prob))
+  neighbourFreqs <- table(unlist(neighbours))/classData$trainFreqs[names(table(unlist(neighbours)))]
+  neighbourScores <- apply(neighbourMat,2,sum,na.rm=T)
+  neighbourScores <- neighbourScores/max(neighbourScores)
+  return(list("results"=res,"neighbours"=neighbourScores))
+}
+
 
 
